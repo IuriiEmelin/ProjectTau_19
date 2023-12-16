f070876c817695564e7fb0c563d32e895b669989
Iurii <Iurii>
2023-12-16 15:13:21 +0500

    Initial Commit

---
+++ ProjectTau/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectTau
+//
+//  Created by Юрий Емелин on 16.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
